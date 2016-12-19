300c469842757bfb5fbe8ea958fc720625e0cb62
Michihiro Takashi <michihiro@localhost.localdomain>
2016-12-19 02:01:16 -0500

    added some codes

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/tkshmichihiro/stringutil"
+)
 
-func main () {
-     fmt.Printf("Hello, World\n")
+func main() {
+	fmt.Printf("Hello, World\n")
+	fmt.Printf(stringutil.Reverse("!oG, olleH"))
 }
